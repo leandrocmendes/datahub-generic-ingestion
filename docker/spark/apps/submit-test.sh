01242b902c315d4a265aa53c30df02ebd3e438df
costa <leandrocosta@outlook.com.br>
2022-10-03 12:03:52 -0300

    adicionando massa de dados no formato avro

--- docker/spark/apps/submit-test.sh
+++ docker/spark/apps/submit-test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-/opt/spark/bin/spark-submit \
---class com.br.datahub.generic.ingestion.Main \
---master local \
---name test \
---files /opt/spark-data/ingestion-configuration-csv.yml \
---jars /opt/spark-apps/mysql-connector-java-8.0.30.jar \
-./datahub-generic-ingestion-1.0-SNAPSHOT.jar ingestion-configuration-csv.yml
+#/opt/spark/bin/spark-submit \
+#--class com.br.datahub.generic.ingestion.Main \
+#--master local \
+#--name test \
+#--files /opt/spark-data/ingestion-configuration-csv.yml \
+#--jars /opt/spark-apps/mysql-connector-java-8.0.30.jar \
+#./datahub-generic-ingestion-1.0-SNAPSHOT.jar ingestion-configuration-csv.yml
 
 #/opt/spark/bin/spark-submit \
 #--class com.br.datahub.generic.ingestion.Main \
@@ -14,3 +14,12 @@
 #--files /opt/spark-data/ingestion-configuration-parquet.yml \
 #--jars /opt/spark-apps/mysql-connector-java-8.0.30.jar \
 #./datahub-generic-ingestion-1.0-SNAPSHOT.jar ingestion-configuration-parquet.yml
+
+/opt/spark/bin/spark-submit \
+--class com.br.datahub.generic.ingestion.Main \
+--master local \
+--name test-avro \
+--files /opt/spark-data/ingestion-configuration-avro.yml \
+--jars /opt/spark-apps/mysql-connector-java-8.0.30.jar \
+--packages org.apache.spark:spark-avro_2.12:3.0.0 \
+./datahub-generic-ingestion-1.0-SNAPSHOT.jar ingestion-configuration-avro.yml
